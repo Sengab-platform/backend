51db795d05ba63a0138df0dd3315a8f945db76ca
ahmad <ahmad.melegy@gmail.com>
2016-03-23 16:21:54 +0200

    Add StatsRetriever Actor

---
+++ app/actors/StatsRetriever.scala
@@ -0,0 +1,14 @@
+package actors
+
+import akka.actor.{Actor, Props}
+import messages.ProjectMangerMessages.GetProjectStats
+
+class StatsRetriever extends Actor {
+  override def receive = {
+    case GetProjectStats(projectID) => ???
+  }
+}
+
+object StatsRetriever {
+  def props(): Props = Props(new StatsRetriever)
+}
