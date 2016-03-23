2644bffdddfdbe96f0c8bd3b1f16a04aba6bec04
ahmad <ahmad.melegy@gmail.com>
2016-03-23 15:36:41 +0200

    Update ProjectManagerMessages

--- app/messages/ProjectMangerMessages.scala
+++ app/messages/ProjectMangerMessages.scala
@@ -6,6 +6,18 @@ object ProjectMangerMessages {
 
   trait ProjectMessage
 
-  case class CreateProject(p: Project) extends ProjectMessage
+  case class CreateProject(project: Project) extends ProjectMessage
+
+  case class ListProjects(filter: String, offset: Integer, limit: Integer) extends ProjectMessage
+
+  case class GetProjectDetails(projectID: Integer) extends ProjectMessage
+
+  case class GetProjectResults(projectID: Integer, offset: Integer, limit: Integer) extends ProjectMessage
+
+  case class GetProjectStats(projectID: Integer) extends ProjectMessage
+
+  case class SearchProjects(keyword: String) extends ProjectMessage
+
+  case class ValidateProject(project: Project) extends ProjectMessage
 
 }
