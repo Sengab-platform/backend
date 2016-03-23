37affc21a5f286434b0453b564da7529ac07d68b
ahmad <ahmad.melegy@gmail.com>
2016-03-23 15:45:51 +0200

    Update ProjectManager

---
+++ app/actors/ProjectManager.scala
@@ -0,0 +1,25 @@
+package actors
+
+import akka.actor.{Actor, Props}
+import messages.ProjectMangerMessages._
+
+class ProjectManager extends Actor {
+  override def receive = {
+    case CreateProject(project) => ???
+
+    case ListProjects(filter, offset, limit) => ???
+
+    case GetProjectDetails(projectID) => ???
+
+    case GetProjectResults(projectID, offset, limit) => ???
+
+    case GetProjectStats(projectID) => ???
+
+    case SearchProjects(keyword) => ???
+
+  }
+}
+
+object ProjectManager {
+  def props(): Props = Props(new ProjectManager)
+}
