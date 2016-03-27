b9efd5eaec354cd4f95e148a51dc1fc085ca4964
AmrElmasry <amrelmasry.me@gmail.com>
2016-03-28 00:55:06 +0200

    Handle Add project request

--- app/actors/project/ProjectManager.scala
+++ app/actors/project/ProjectManager.scala
@@ -2,13 +2,16 @@ package actors.project
 
 import akka.actor.{Actor, Props}
 import messages.ProjectManagerMessages._
+import play.api.Logger
 
 class ProjectManager extends Actor {
 
-  val validator = context.actorOf(ProjectValidator.props(), "assa")
+  val projectValidator = context.actorOf(ProjectValidator.props(), "projectValidator")
 
   override def receive = {
-    case CreateProject(project, userID) => ???
+    case CreateProject(project, userID) =>
+      Logger.info(s"actor ${self.path} - received msg : ${CreateProject(project, userID)} ")
+      projectValidator forward ValidateProject(project, userID)
 
     case ListProjects(filter, offset, limit) => ???
 
--- app/models/responses/Error.scala
+++ app/models/responses/Error.scala
@@ -1,7 +1,14 @@
 package models.responses
 
+import play.api.libs.json.Json
 import play.api.mvc.Result
 
 case class Error(result: Result)
 
-case class ErrorMsg(msg: String, devMsg: String)
+
+case class ErrorMsg(msg: String, devMsg: String) {
+
+  implicit val ErrorMsgF = Json.writes[ErrorMsg]
+
+  def toJson = Json.toJson(this)
+}
