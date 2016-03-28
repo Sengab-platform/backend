20bc6da9dc52d7fa14b1c5078623611df45fbe47
ahmad <ahmad.melegy@gmail.com>
2016-03-28 02:18:12 +0200

    Handle Get User Info

--- app/actors/user/InfoRetriever.scala
+++ app/actors/user/InfoRetriever.scala
@@ -2,10 +2,23 @@ package actors.user
 
 import akka.actor.{Actor, Props}
 import messages.UserManagerMessages.GetUserProfile
+import models.responses.Response
+import play.Logger
+import play.api.libs.json.Json
 
 class InfoRetriever extends Actor {
   override def receive = {
-    case GetUserProfile(userID) => ???
+
+    case GetUserProfile(userID) =>
+
+      Logger.info(s"actor ${self.path} - received msg : ${GetUserProfile(userID)} ")
+
+      // Here we will send the result
+      sender() ! Response(Json.toJson("user info retrieved successfully"))
+
+      // Kill infoRetriever
+      context stop sender()
+
   }
 }
 
--- app/actors/user/UserManager.scala
+++ app/actors/user/UserManager.scala
@@ -2,15 +2,23 @@ package actors.user
 
 import akka.actor.{Actor, Props}
 import messages.UserManagerMessages.{GetUserProfile, ListProjectsOfUser, ListUserActivity}
+import play.Logger
 
 class UserManager extends Actor {
+
+  val userRetriever = context.actorOf(UserRetriever.props(), "userRetriever")
+
   override def receive = {
-    case GetUserProfile(userID) => ???
+    case GetUserProfile(userID) =>
+
+      Logger.info(s"actor ${self.path} - received msg : ${GetUserProfile(userID)} ")
+
+      // forward message to UserRetriever
+      userRetriever forward GetUserProfile(userID)
 
     case ListUserActivity(userID, offset, limit) => ???
 
     case ListProjectsOfUser(userID, sort, offset, limit) => ???
-
   }
 }
 
