ad7415396b569d3004da3b6555e4fa660dd479df
ahmad <ahmad.melegy@gmail.com>
2016-03-29 00:51:58 +0200

    Add User Model for Google Authentication

---
+++ app/auth/UserAuth.scala
@@ -0,0 +1,23 @@
+package auth
+
+import models.User
+import org.joda.time.DateTime
+import securesocial.core._
+
+case class UserAuth(main: BasicProfile, gender: Option[String], bio: Option[String], identities: List[BasicProfile]) {
+  create(main: BasicProfile, gender: Option[String], bio: Option[String], identities: List[BasicProfile])
+
+  def create(main: BasicProfile, gender: Option[String], bio: Option[String], identities: List[BasicProfile]) {
+    val userID = "user::" + main.userId
+    val entity_type = "user"
+    val first_name = main.firstName
+    val last_name = main.lastName
+    val image = main.avatarUrl
+    val about = ???
+    val stats: Map[String, Int] = Map("projects" -> 0, "contributions" -> 0)
+    val dateTime: DateTime = DateTime.now
+    val created_at = dateTime.toString
+
+    User(userID, entity_type, first_name, last_name, image, about, stats, created_at)
+  }
+}
