83a48e9dd92077f8bce43488fdf19e7d5270c520
ahmad <ahmad.melegy@gmail.com>
2016-03-29 01:30:35 +0200

    Add Global Class

---
+++ app/Global.scala
@@ -0,0 +1,17 @@
+
+import auth.models.UserAuth
+import auth.services.AuthUserService
+import securesocial.core.RuntimeEnvironment
+
+object Global extends play.api.GlobalSettings {
+
+  /**
+    * Application's custom Runtime Environment
+    */
+  object MyRuntimeEnvironment extends RuntimeEnvironment.Default {
+    type U = UserAuth
+    override lazy val userService: AuthUserService = new AuthUserService()
+    override implicit val executionContext = play.api.libs.concurrent.Execution.defaultContext
+  }
+
+}
