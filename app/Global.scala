34350b98079b3432070ac9fdcaadcb304f5b0dd0
ahmad <ahmad.melegy@gmail.com>
2016-03-29 03:15:44 +0200

    Add Google Provider

--- app/Global.scala
+++ app/Global.scala
@@ -2,6 +2,9 @@
 import auth.models.UserAuth
 import auth.services.AuthUserService
 import securesocial.core.RuntimeEnvironment
+import securesocial.core.providers.GoogleProvider
+
+import scala.collection.immutable.ListMap
 
 object Global extends play.api.GlobalSettings {
 
@@ -11,6 +14,8 @@ object Global extends play.api.GlobalSettings {
   object MyRuntimeEnvironment extends RuntimeEnvironment.Default {
     type U = UserAuth
     override lazy val userService: AuthUserService = new AuthUserService()
+    override lazy val providers = ListMap(
+      include(new GoogleProvider(routes, cacheService, oauth2ClientFor(GoogleProvider.Google))))
     override implicit val executionContext = play.api.libs.concurrent.Execution.defaultContext
   }
 
