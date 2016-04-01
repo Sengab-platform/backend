981a2364202231ec44928ff8b232a941709e4f11
ahmad <ahmad.melegy@gmail.com>
2016-04-02 01:02:25 +0200

    Add `Contribution` model

--- app/models/Contribution.scala
+++ app/models/Contribution.scala
@@ -1,3 +1,18 @@
 package models
 
-case class Contribution()
+import models.project.Project
+import play.api.libs.json.{JsValue, Json}
+
+case class Contribution(
+                         id: String,
+                         url: String,
+                         project: Project,
+                         contributor: User,
+                         created_at: String,
+                         data: JsValue
+                       )
+
+object Contribution {
+  implicit val ContributionF = Json.format[Contribution]
+
+}
