805685425652fa53134e0c13dcf688ec05a25af4
ahmad <ahmad.melegy@gmail.com>
2016-04-02 01:01:36 +0200

    Update `Project` model

--- app/models/project/Project.scala
+++ app/models/project/Project.scala
@@ -1,17 +1,39 @@
 package models.project
 
+import models.Category
 import play.api.libs.json.Json
 
-case class Project(name: String,
-                   brief_description: String,
-                   detailed_description: String,
-                   goal: Int,
-                   is_featured: Boolean,
-                   created_at: String,
-                   category_id: Int,
-                   template_id: Int,
-                   templateBody: TemplateBody) {
+case class Owner(
+                  id: String,
+                  url: String,
+                  name: String
+                )
+
+object Owner {
+  implicit val OwnerF = Json.format[Owner]
+
 }
+
+case class Project(
+                    id: Option[String] = None,
+                    url: Option[String] = None,
+                    name: String,
+                    image: String,
+                    owner: Option[Owner] = None,
+                    goal: Int,
+                    template_id: Option[Int] = None,
+                    templateBody: Option[TemplateBody] = None,
+                    created_at: String,
+                    brief_description: String,
+                    detailed_description: String,
+                    enrollments_count: Option[String] = None,
+                    contributions_count: Option[String] = None,
+                    is_featured: Option[Boolean] = None,
+                    category_id: Option[Int] = None,
+                    category: Category,
+                    results: String,
+                    stats: String
+                  )
 
 object Project {
   implicit val ProjectF = Json.format[Project]
