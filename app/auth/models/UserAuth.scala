9454fb551eaa8ad49ff79b8e4611a81812942a0c
ahmad <ahmad.melegy@gmail.com>
2016-04-02 00:20:57 +0200

    Update UserAuth Model to match AuthUserService

--- app/auth/models/UserAuth.scala
+++ app/auth/models/UserAuth.scala
@@ -6,7 +6,7 @@ import org.joda.time.DateTime
 import play.api.libs.json.{JsObject, Json}
 import securesocial.core._
 
-case class UserAuth(main: BasicProfile, gender: Option[String], bio: Option[String], identities: List[BasicProfile]) {
+case class UserAuth(main: BasicProfile, gender: Option[String] = None, bio: Option[String] = None, identities: List[BasicProfile]) {
   create(main: BasicProfile, gender: Option[String], bio: Option[String], identities: List[BasicProfile])
 
   def create(main: BasicProfile, gender: Option[String], bio: Option[String], identities: List[BasicProfile]) {
