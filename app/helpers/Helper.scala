8b2888ce4b30d5e69b26c81428a1c526d23184ea
ahmad <ahmad.melegy@gmail.com>
2016-04-02 12:29:02 +0200

    Update values in `Helper` Class

--- app/helpers/Helper.scala
+++ app/helpers/Helper.scala
@@ -1,9 +1,9 @@
 package helpers
 
 object Helper {
-  val STAT_PATH = "sengab.com/v1/stats/"
-  val RESULT_PATH = "sengab.com/v1/results/"
-  val CATEGORY_PATH = "sengab.com/v1/categories/"
-  val PROJECT_PATH = "sengab.com/v1/projects/"
-  val USER_PATH = "sengab.com/v1/users/"
+  val STAT_PATH = "http://api.sengab.com/v1/stats/"
+  val RESULT_PATH = "http://api.sengab.com/v1/results/"
+  val CATEGORY_PATH = "http://api.sengab.com/v1/categories/"
+  val PROJECT_PATH = "http://api.sengab.com/v1/projects/"
+  val USER_PATH = "http://api.sengab.com/v1/users/"
 }
