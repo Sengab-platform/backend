0dab798c2e9e94eede0e498d32d6b93544f37c89
RadwaKamal <clumsyshark@gmail.com>
2016-04-17 17:06:25 +0200

    Handle Enrollment removal side effect

--- app/actors/enrollment/EnrollmentManager.scala
+++ app/actors/enrollment/EnrollmentManager.scala
@@ -2,20 +2,19 @@ package actors.enrollment
 
 import akka.actor.{Actor, Props}
 import messages.EnrollmentManagerMessages.{Enroll, Withdraw}
-import models.Enrollment
 import play.api.Logger
 
 class EnrollmentManager extends Actor {
   override def receive = {
-    case Enroll(userID: String, projectID: Enrollment) =>
-      Logger.info(s"actor ${self.path} - received msg : ${Enroll(userID: String, projectID: Enrollment)}")
+    case Enroll(userID: String, projectID: String) =>
+      Logger.info(s"actor ${self.path} - received msg : ${Enroll(userID: String, projectID: String)}")
       val enrollmentHandler = context.actorOf(EnrollmentHandler.props(sender()))
-      enrollmentHandler forward Enroll(userID: String, projectID: Enrollment)
+      enrollmentHandler forward Enroll(userID: String, projectID: String)
 
-    case Withdraw(userID: String, projectID: Enrollment) =>
-      Logger.info(s"actor ${self.path} - received msg : ${Withdraw(userID: String, projectID: Enrollment)}")
+    case Withdraw(userID: String, projectID: String) =>
+      Logger.info(s"actor ${self.path} - received msg : ${Withdraw(userID: String, projectID: String)}")
       val withdrawHandler = context.actorOf(WithdrawHandler.props(sender()))
-      withdrawHandler forward Withdraw(userID: String, projectID: Enrollment)
+      withdrawHandler forward Withdraw(userID: String, projectID: String)
   }
 }
 
