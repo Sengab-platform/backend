93f46815d4a40327d45c45b7ba8d1ad2ae559264
AmrElmasry <amrelmasry.me@gmail.com>
2016-04-18 17:55:46 +0200

    Add Names for DB Actors for more readable logging

--- app/actors/enrollment/EnrollmentManager.scala
+++ app/actors/enrollment/EnrollmentManager.scala
@@ -8,12 +8,12 @@ class EnrollmentManager extends Actor {
   override def receive = {
     case Enroll(userID: String, projectID: String) =>
       Logger.info(s"actor ${self.path} - received msg : ${Enroll(userID: String, projectID: String)}")
-      val enrollmentHandler = context.actorOf(EnrollmentHandler.props(sender()))
+      val enrollmentHandler = context.actorOf(EnrollmentHandler.props(sender()), "enrollmentHandler")
       enrollmentHandler forward Enroll(userID: String, projectID: String)
 
     case Withdraw(userID: String, projectID: String) =>
       Logger.info(s"actor ${self.path} - received msg : ${Withdraw(userID: String, projectID: String)}")
-      val withdrawHandler = context.actorOf(WithdrawHandler.props(sender()))
+      val withdrawHandler = context.actorOf(WithdrawHandler.props(sender()), "withdrawHandler")
       withdrawHandler forward Withdraw(userID: String, projectID: String)
   }
 }
