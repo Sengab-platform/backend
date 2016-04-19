5f2e44e839ff4ca834b0206ab4a17c73d5464e54
ahmad <ahmad.melegy@gmail.com>
2016-04-19 15:19:56 +0200

    Add user not found and no activities found cases in testing getting user activities

--- test/user/UserActivitiesRetrievementSpec.scala
+++ test/user/UserActivitiesRetrievementSpec.scala
@@ -2,20 +2,33 @@ package user
 
 import core.AbstractSpec
 import messages.UserManagerMessages.ListUserActivity
+import models.errors.GeneralErrors.NotFoundError
 import models.{Activities, Response}
 
 class UserActivitiesRetrievementSpec extends AbstractSpec {
 
   //Test `ListUserActivity` message
+
+  // If the user has some activities
   "Receptionist Actor" should "Get User Activities Successfully" in {
     // `ListUserActivity` message takes the ID of the activity, the trimmed ID of the user
-    receptionist ! ListUserActivity("117521628211683444029", 0, 20)
+    receptionist ! ListUserActivity("5", 0, 20)
     val response = expectMsgType[Response]
-    // If the user has some activities, then the response will validate `Activities` model
-    assert(response.jsonResult.validate[Activities].isSuccess
-      // if not it will be equal to an empty array
-      //todo may be there is a better way than converting to string
-      || response.jsonResult.toString == "[]")
+    // the response have to validate `Activities` model
+    assert(response.jsonResult.validate[Seq[Activities]].isSuccess)
   }
 
+  // If the user does not exist
+  it should "Get empty document" in {
+    receptionist ! ListUserActivity("invalid-id", 0, 20)
+    expectMsgType[NotFoundError]
+  }
+
+  // If the user does not have activities
+  it should "Get empty json array" in {
+    receptionist ! ListUserActivity("117521628211683444029", 0, 20)
+    val response = expectMsgType[Response]
+    // the response will be an empty json array
+    assert(response.jsonResult.toString == "[]")
+  }
 }
