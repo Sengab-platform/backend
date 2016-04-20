3e88aa66550c04fcb6927babc0d476e3b9a6d410
ahmad <ahmad.melegy@gmail.com>
2016-04-20 16:47:33 +0200

    Handle not found project error in `Enroll` message

--- app/actors/enrollment/EnrollmentHandler.scala
+++ app/actors/enrollment/EnrollmentHandler.scala
@@ -8,8 +8,8 @@ import helpers.Helper
 import helpers.Helper._
 import messages.EnrollmentManagerMessages.Enroll
 import models.Response
-import models.errors.GeneralErrors.{AlreadyExists, CouldNotParseJSON}
-import play.api.Logger
+import models.errors.GeneralErrors.{AlreadyExists, CouldNotParseJSON, NotFoundError}
+import play.Logger
 import play.api.libs.json.{JsObject, JsString, Json}
 
 
@@ -24,27 +24,33 @@ class EnrollmentHandler(out: ActorRef) extends AbstractDBHandler(out) {
       executeQuery(DBUtilities.User.addProjectToEnrolledProjects(userID, projectID))
 
     case QueryResult(jsonObject) =>
-      if (jsonObject.containsKey("projectId")) {
-        val response = constructResponse(jsonObject)
-        response match {
-          case Some(Response(jsonResult)) =>
-            // apply side effects
+      Logger.info(s"actor ${self.path} - received msg : ${QueryResult(jsonObject)} ")
+      if (jsonObject.getString("id") != DBUtilities.DBConfig.EMPTY_JSON_OBJECT) {
 
-            // get project id as String
-            val projectID = (jsonResult \ "project_id").as[String]
-            executeQuery(DBUtilities.Project.add1ToProjectEnrollmentsCount(projectID))
-            // get statsID
-            val statsID = Helper.StatsIDPrefix + trimEntityID(projectID)
-            executeQuery(DBUtilities.Stats.add1ToStatsEnrollmentsCount(statsID))
-            out ! Response(jsonResult)
+        if (jsonObject.containsKey("projectId")) {
+          val response = constructResponse(jsonObject)
+          response match {
+            case Some(Response(jsonResult)) =>
+              // apply side effects
 
-          case None =>
-            out ! CouldNotParseJSON("failed to enroll user",
-              "couldn't parse json retrieved from the db ", this.getClass.toString)
+              // get project id as String
+              val projectID = (jsonResult \ "project_id").as[String]
+              executeQuery(DBUtilities.Project.add1ToProjectEnrollmentsCount(projectID))
+              // get statsID
+              val statsID = Helper.StatsIDPrefix + trimEntityID(projectID)
+              executeQuery(DBUtilities.Stats.add1ToStatsEnrollmentsCount(statsID))
+              out ! Response(jsonResult)
+
+            case None =>
+              out ! CouldNotParseJSON("failed to enroll user",
+                "couldn't parse json retrieved from the db ", this.getClass.toString)
+          }
         }
-      }
-      else {
-        out ! AlreadyExists("You are already enrolled to this project", "User's already enrolled to this project", this.getClass.toString)
+        else if (jsonObject.getString("id") == DBUtilities.DBConfig.ALREADY_ENROLLED) {
+          out ! AlreadyExists("You are already enrolled to this project", "User's already enrolled to this project", this.getClass.toString)
+        } else if (jsonObject.getString("id") == DBUtilities.DBConfig.NO_SUCH_PROJECT) {
+          out ! NotFoundError("No such project", s"No such project with this ID", this.getClass.toString)
+        }
       }
   }
 
