bc9138b11d28a45a0f9fcbe913a337ba2262e5d6
ahmad <ahmad.melegy@gmail.com>
2016-04-21 23:28:32 +0200

    Add test cases of creating project of the 4 template types

--- test/project/ProjectCreationSpec.scala
+++ test/project/ProjectCreationSpec.scala
@@ -9,9 +9,23 @@ class ProjectCreationSpec extends AbstractSpec {
 
   // test Create Project Request
 
-  "Receptionist Actor" should "Create Project Successfully" in {
-    val newProject = Constants.ValidProject
-    receptionist ! CreateProject(newProject, Constants.ValidUserID)
+  "Receptionist Actor" should "Create Project of template 1 Successfully" in {
+    receptionist ! CreateProject(Constants.ValidProjectTemplateOne, Constants.ValidUserID)
+    expectMsgType[Response]
+  }
+
+  it should "Create Project of template 2 Successfully" in {
+    receptionist ! CreateProject(Constants.ValidProjectTemplateTwo, Constants.ValidUserID)
+    expectMsgType[Response]
+  }
+
+  it should "Create Project of template 3 Successfully" in {
+    receptionist ! CreateProject(Constants.ValidProjectTemplateThree, Constants.ValidUserID)
+    expectMsgType[Response]
+  }
+
+  it should "Create Project of template 4 Successfully" in {
+    receptionist ! CreateProject(Constants.ValidProjectTemplateFour, Constants.ValidUserID)
     expectMsgType[Response]
   }
 }
--- test/utils/Constants.scala
+++ test/utils/Constants.scala
@@ -1,16 +1,19 @@
 package utils
 
 import models.project.Project.NewProject
-import models.project.Templates.TemplateOne
+import models.project.Templates._
 
 object Constants {
 
-  val ValidUserID = "user::117521628211683444029"
   val InvalidID = "invalid-id"
+
+  val ValidUserID = "user::117521628211683444029"
 
   val ValidProjectID = "project::1"
   val NewProjectID = "project::5"
-  val ValidProject = NewProject("Recognize",
+
+  val ValidProjectTemplateOne = NewProject(
+    "Project of template 1",
     2000,
     "image",
     1,
@@ -18,9 +21,45 @@ object Constants {
     "2015",
     "short",
     "detailed one",
+    "category::1")
+
+  val ValidProjectTemplateTwo = NewProject(
+    "Project of template 2",
+    2000,
+    "image",
+    2,
+    TemplateTwo("Image title?"),
+    "2015",
+    "short",
+    "detailed one",
     "category::5")
+
+  val ValidProjectTemplateThree = NewProject(
+    "Project of template 3",
+    2000,
+    "image",
+    3,
+    TemplateThree(List(
+      new Question("1", "what?"), new Question("2", "why?"))),
+    "2015",
+    "short",
+    "detailed one",
+    "category::5")
+
+  val ValidProjectTemplateFour = NewProject(
+    "Project of template 4",
+    2000,
+    "image",
+    4,
+    TemplateFour("image title"),
+    "2015",
+    "short",
+    "detailed one",
+    "category::5")
+
   val ValidSearchKeyword = "recognize"
   val InvalidSearchKeyword = "dummytexttoinsureitwillfail"
+
   val ValidProjectToEnroll = "project::2"
   val AlreadyEnrolledProject = ValidProjectID
 
