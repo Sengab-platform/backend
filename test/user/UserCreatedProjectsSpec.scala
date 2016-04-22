c6e4b89d448c80a2f52c6c8316fba0df2b8011e9
ahmad <ahmad.melegy@gmail.com>
2016-04-23 01:58:55 +0200

    Add test case when user has not created any projects

--- test/user/UserCreatedProjectsSpec.scala
+++ test/user/UserCreatedProjectsSpec.scala
@@ -17,8 +17,14 @@ class UserCreatedProjectsSpec extends AbstractSpec {
     assert(response.jsonResult.validate[Seq[EmbeddedProject]].isSuccess)
   }
 
-  // User have not created any projects
-  it should "Return NOT FOUND error" in {
+  // User has not created any projects
+  it should "Return NOT FOUND error as user has not created any projects" in {
+    receptionist ! ListProjectsOfUser(Constants.UserIDWithNoCreatedProjects, Helper.CreatedKeyword, 0, 20)
+    expectMsgType[NotFoundError]
+  }
+
+  // User not found
+  it should "Return NOT FOUND error as the user is not existing" in {
     receptionist ! ListProjectsOfUser(Constants.InvalidID, Helper.CreatedKeyword, 0, 20)
     expectMsgType[NotFoundError]
   }
