d3042b233868fd6265204427b698881edd831ae0
ahmad <ahmad.melegy@gmail.com>
2016-04-23 02:03:34 +0200

    Add test case when the user tries to list stats of project of template 2,3 and 4

--- test/project/ProjectStatsRetrievement.scala
+++ test/project/ProjectStatsRetrievement.scala
@@ -7,9 +7,28 @@ import models.{Response, Stats}
 import utils.Constants
 
 class ProjectStatsRetrievement extends AbstractSpec {
+
   // The project has results
-  "Receptionist Actor" should "Return Project Stats Successfully" in {
-    receptionist ! GetProjectStats(Constants.ValidProjectID)
+  "Receptionist Actor" should "Return stats for project of template 1 successfully" in {
+    receptionist ! GetProjectStats(Constants.ProjectIDOfTemplate1)
+    val response = expectMsgType[Response]
+    assert(response.jsonResult.validate[Stats].isSuccess)
+  }
+
+  it should "Return stats for project of template 2 successfully" in {
+    receptionist ! GetProjectStats(Constants.ProjectIDOfTemplate2)
+    val response = expectMsgType[Response]
+    assert(response.jsonResult.validate[Stats].isSuccess)
+  }
+
+  it should "Return stats for project of template 3 successfully" in {
+    receptionist ! GetProjectStats(Constants.ProjectIDOfTemplate3)
+    val response = expectMsgType[Response]
+    assert(response.jsonResult.validate[Stats].isSuccess)
+  }
+
+  it should "Return stats for project of template 4 successfully" in {
+    receptionist ! GetProjectStats(Constants.ProjectIDOfTemplate4)
     val response = expectMsgType[Response]
     assert(response.jsonResult.validate[Stats].isSuccess)
   }
